439f39119ed9bc2bbc8282fb40e949cdf42b58f9
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-08-21 16:58:55 +0900

    feat: Implement Rs232Stream as library with test

--- app/mk5-serial0/src/main.cpp
+++ app/mk5-serial0/src/main.cpp
@@ -1,5 +1,3 @@
-#include "efp.hpp"
-#include "serialib.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <iostream>
@@ -7,6 +5,9 @@
 #include <bitset>
 #include <thread>
 #include <chrono>
+
+#include "efp.hpp"
+#include "rs232-stream.hpp"
 
 using namespace efp;
 
@@ -29,104 +30,6 @@ struct IntPair
     int b;
 };
 
-template <typename A, typename F>
-struct Rs232Stream
-{
-public:
-    Rs232Stream(const char *device, const unsigned int baudrate, const F &on_read)
-        : device_(device), baudrate_(baudrate), on_read_(on_read)
-    {
-        read_period_us_ = sizeof(A) / (baudrate / 8.) * 1000000;
-        // read_period_us_ = 1000000;
-
-        // ! Possible stack overflow
-        thread_ = std::thread(&Rs232Stream::on_disconnect_, this);
-    }
-
-    ~Rs232Stream()
-    {
-        keep_alive_ = false;
-        serial_.closeDevice();
-    }
-
-private:
-    void on_connect_()
-    {
-        std::cout << "connected" << std::endl;
-
-        while (serial_.isDeviceOpen() && keep_alive_)
-        {
-            auto start = std::chrono::high_resolution_clock::now();
-
-            uint8_t start_byte = 0;
-            int removed_bytes = 0;
-            while (start_byte != 0x55 && (removed_bytes < 2 * sizeof(A)))
-            {
-                serial_.readBytes(&start_byte, 1);
-                removed_bytes++;
-            }
-
-            // std::cout << "number of removed bytes: " << removed_bytes << std::endl;
-
-            if (removed_bytes < 2 * sizeof(A))
-            {
-                uint8_t rx_buffer[sizeof(A)];
-                serial_.readBytes(&rx_buffer, sizeof(A));
-                A rx_data = *(A *)(&rx_buffer);
-
-                on_read_(rx_data);
-            }
-            else
-            {
-                std::cout << "too many illigal bytes" << std::endl;
-                serial_.closeDevice();
-            }
-
-            auto end = std::chrono::high_resolution_clock::now();
-            auto elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-            auto remaining_time = std::chrono::microseconds(read_period_us_) - elapsed_time;
-            if (remaining_time > std::chrono::microseconds(0))
-            {
-                std::this_thread::sleep_for(remaining_time);
-            }
-        }
-
-        on_disconnect_();
-    }
-
-    void on_disconnect_()
-    {
-        std::cout << "disconnected" << std::endl;
-
-        while (!serial_.isDeviceOpen() && keep_alive_)
-        {
-            std::cout << "awaiting for connection" << std::endl;
-
-            char res = serial_.openDevice(device_, baudrate_);
-            if (res != 1)
-            {
-                // ! temp 1000, original 50
-                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-            }
-            else
-            {
-                on_connect_();
-            }
-        }
-
-        std::cout << "terminating" << std::endl;
-    }
-
-    serialib serial_;
-    const char *device_;
-    const unsigned int baudrate_;
-    const F on_read_;
-
-    int read_period_us_;
-    bool keep_alive_ = true;
-    std::thread thread_;
-};
-
 int main(int argc, char const *argv[])
 {
     const char *serial_port = "/dev/cu.usbserial-0001";
@@ -134,7 +37,6 @@ int main(int argc, char const *argv[])
 
     auto on_read = [](const SerialFrame &data)
     {
-        // std::cout << "len max30102: " << data.max30102_data.num_data << std::endl;
         std::cout << "red: " << data.max30102_data.red
                   << " ir: " << data.max30102_data.ir
                   << " temp: " << data.max30102_data.temp << std::endl;
@@ -144,12 +46,6 @@ int main(int argc, char const *argv[])
         packet_stream{serial_port, baudrate, on_read};
 
     assert(sizeof(SerialFrame) == 140);
-
-    // auto on_read = [](const IntPair &data)
-    // { std::cout << "a: " << data.a << "b: " << data.b << std::endl; };
-
-    // Rs232Stream<IntPair, decltype(on_read)>
-    //     packet_stream{serial_port, baudrate, on_read};
 
     while (true)
     {
---
+++ lib/rs232-stream/include/rs232-stream.hpp
@@ -0,0 +1,104 @@
+#ifndef RS232_STREAM_HPP_
+#define RS232_STREAM_HPP_
+
+#include "serialib.h"
+
+template <typename A, typename F>
+struct Rs232Stream
+{
+public:
+    Rs232Stream(const char *device, const unsigned int baudrate, const F &on_read)
+        : device_(device), baudrate_(baudrate), on_read_(on_read)
+    {
+        read_period_us_ = sizeof(A) / (baudrate / 8.) * 1000000;
+        // read_period_us_ = 1000000;
+
+        // ! Possible stack overflow
+        thread_ = std::thread(&Rs232Stream::on_disconnect_, this);
+    }
+
+    ~Rs232Stream()
+    {
+        keep_alive_ = false;
+        serial_.closeDevice();
+    }
+
+private:
+    void on_connect_()
+    {
+        std::cout << "connected" << std::endl;
+
+        while (serial_.isDeviceOpen() && keep_alive_)
+        {
+            auto start = std::chrono::high_resolution_clock::now();
+
+            uint8_t start_byte = 0;
+            int removed_bytes = 0;
+            while (start_byte != 0x55 && (removed_bytes < 2 * sizeof(A)))
+            {
+                serial_.readBytes(&start_byte, 1);
+                removed_bytes++;
+            }
+
+            // std::cout << "number of removed bytes: " << removed_bytes << std::endl;
+
+            if (removed_bytes < 2 * sizeof(A))
+            {
+                uint8_t rx_buffer[sizeof(A)];
+                serial_.readBytes(&rx_buffer, sizeof(A));
+                A rx_data = *(A *)(&rx_buffer);
+
+                on_read_(rx_data);
+            }
+            else
+            {
+                std::cout << "too many illigal bytes" << std::endl;
+                serial_.closeDevice();
+            }
+
+            auto end = std::chrono::high_resolution_clock::now();
+            auto elapsed_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+            auto remaining_time = std::chrono::microseconds(read_period_us_) - elapsed_time;
+            if (remaining_time > std::chrono::microseconds(0))
+            {
+                std::this_thread::sleep_for(remaining_time);
+            }
+        }
+
+        on_disconnect_();
+    }
+
+    void on_disconnect_()
+    {
+        std::cout << "disconnected" << std::endl;
+
+        while (!serial_.isDeviceOpen() && keep_alive_)
+        {
+            std::cout << "awaiting for connection" << std::endl;
+
+            char res = serial_.openDevice(device_, baudrate_);
+            if (res != 1)
+            {
+                // ! temp 1000, original 50
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            }
+            else
+            {
+                on_connect_();
+            }
+        }
+
+        std::cout << "terminating" << std::endl;
+    }
+
+    serialib serial_;
+    const char *device_;
+    const unsigned int baudrate_;
+    const F on_read_;
+
+    int read_period_us_;
+    bool keep_alive_ = true;
+    std::thread thread_;
+};
+
+#endif
